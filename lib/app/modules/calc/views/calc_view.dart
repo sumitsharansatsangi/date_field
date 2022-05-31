9df8356a1364406b305e5a3d042b7db5d49e4ea9
sumitsharansatsangi <sharansumitkumar@gmail.com>
2022-05-31 13:53:11 +0530

    calculator gui corrected

--- lib/app/modules/calc/views/calc_view.dart
+++ lib/app/modules/calc/views/calc_view.dart
@@ -9,153 +9,54 @@ import '../controllers/calc_controller.dart';
 class CalcView extends GetView<CalcController> {
   @override
   Widget build(BuildContext context) {
-    return OrientationBuilder(builder: (context, orientation) {
-      if (orientation == Orientation.portrait) {
-        return SingleChildScrollView(
-          child: Column(children: [
-            CustomBack(),
-            SizedBox(
-              width: 300.w,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Obx(
-                    () => AutoSizeText(
-                      controller.input.value,
-                      maxLines: 5,
-                      style: TextStyle(
-                          fontSize: 14, color: Colors.deepPurple.shade600),
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Obx(() => SelectableText(controller.output.value,
-                        style: TextStyle(
-                            fontSize: 22.sp,
-                            color: Colors.deepPurple.shade900))),
-                  ),
-                ],
+    return SingleChildScrollView(
+      child: Column(children: [
+        CustomBack(),
+        SizedBox(
+          width: 300.w,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Obx(
+                () => AutoSizeText(
+                  controller.input.value,
+                  maxLines: 5,
+                  style: TextStyle(
+                      fontSize: 14, color: Colors.deepPurple.shade600),
+                ),
               ),
-            ),
-            SizedBox(
-              height: min(400.r, 75.r * 5),
-              width: min(400.r, 65.r * 4),
-              child: GridView.count(
-                physics: const NeverScrollableScrollPhysics(),
-                crossAxisCount: 4,
-                padding: EdgeInsets.symmetric(vertical: 10.sp),
-                children: List.generate(controller.buttons.length, (index) {
-                  return CustomButton(
-                    button: controller.buttons[index],
-                    index: index,
-                    onPressed: () {
-                      controller.calculate(
-                          controller.buttons[index].label, index);
-                    },
-                  );
-                }),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Obx(() => SelectableText(controller.output.value,
+                    style: TextStyle(
+                        fontSize: 22.sp, color: Colors.deepPurple.shade900))),
               ),
-            ),
-          ]),
-        );
-      } else {
-        return Text("jhal modi");
-        // Column(children: [
-        // Row(
-        //   children: [
-        //     CustomBack(),
-        //     Flexible(
-        //         child: SizedBox(
-        //       height: 125.h,
-        //       width: 235.w,
-        //       child: Column(
-        //         mainAxisSize: MainAxisSize.max,
-        //         crossAxisAlignment: CrossAxisAlignment.end,
-        //         mainAxisAlignment: MainAxisAlignment.end,
-        //         children: <Widget>[
-        //           Obx(
-        //             () => AutoSizeText(
-        //               controller.input.value,
-        //               maxLines: 4,
-        //               style: TextStyle(
-        //                   fontSize: 14.sp,
-        //                   color: Colors.deepPurple.shade600),
-        //             ),
-        //           ),
-        //           SingleChildScrollView(
-        //             scrollDirection: Axis.horizontal,
-        //             child: Obx(() => SelectableText(
-        //                 controller.output.value,
-        //                 style: TextStyle(
-        //                     fontSize: 22.sp,
-        //                     color: Colors.deepPurple.shade900))),
-        //           ),
-        //         ],
-        //       ),
-        //     )),
-        //   ],
-        // // ),
-        // SizedBox(
-        //   height: min(350.r, 70.r * 5),
-        //   width: min(350.r, 70.r * 4),
-        //   child: GridView.count(
-        //     physics: const NeverScrollableScrollPhysics(),
-        //     crossAxisCount: 4,
-        //     children: List.generate(controller.buttons.length, (index) {
-        //       return CustomButton(
-        //         button: controller.buttons[index],
-        //         index: index,
-        //         onPressed: () {
-        //           controller.calculate(
-        //               controller.buttons[index].label, index);
-        //         },
-        //       );
-        //     }),
-        //   ),
-        // ),
-        // ]);
-      }
-    });
+            ],
+          ),
+        ),
+        SizedBox(
+          height: min(400.r, 75.r * 5),
+          width: min(400.r, 65.r * 4),
+          child: GridView.count(
+            physics: const NeverScrollableScrollPhysics(),
+            crossAxisCount: 4,
+            padding: EdgeInsets.symmetric(vertical: 10.sp),
+            children: List.generate(controller.buttons.length, (index) {
+              return CustomButton(
+                button: controller.buttons[index],
+                index: index,
+                onPressed: () {
+                  controller.calculate(controller.buttons[index].label, index);
+                },
+              );
+            }),
+          ),
+        ),
+      ]),
+    );
   }
-
-//   calcButton(CalcButton button, int index) {
-//     return Center(
-//       child: SizedBox(
-//         width: 60.r,
-//         height: 60.r,
-//         child: NeumorphicButton(
-//           margin: EdgeInsets.all(2.r),
-//           onPressed: () {
-//             controller.calculate(button.label, index);
-//           },
-//           style: NeumorphicStyle(
-//               depth: 30,
-//               shadowLightColor: Colors.deepPurple.shade900,
-//               surfaceIntensity: 0.35,
-//               boxShape: const NeumorphicBoxShape.circle(),
-//               shape: NeumorphicShape.concave,
-//               color: button.textAccent
-//                   ? Colors.deepPurple.shade900
-//                   : Colors.deepPurple.shade50),
-//           child: Center(
-//             child: FittedBox(
-//               fit: BoxFit.scaleDown,
-//               child: Text(
-//                 button.label,
-//                 style: TextStyle(
-//                     fontSize: 18.sp,
-//                     color: button.textAccent
-//                         ? Colors.deepPurple.shade50
-//                         : Colors.deepPurple.shade900),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
 }
 
 class CustomButton extends StatelessWidget {
